c13a20a3b85a57550708b5076216c3ccfda053fe
Tyler-Scripps <tylerscripps@gmail.com>
2024-02-16 13:01:53 -0700

    basic web functionality

--- vial.py
+++ vial.py
@@ -1,13 +1,21 @@
 class vial:
-    def __init__(self, containerId, searchStr: str, qty) -> None:
-        self.containerId = containerId
+    def __init__(self, containerId, searchStr: str, qty: int) -> None:
+        self._containerId = containerId
         self.qty = qty
         self.attributes = []
         for word in searchStr:
             self.attributes.append(word)
+    
+    def __str__(self) -> str:
+        retStr = ""
+        retStr = retStr + str(self._containerId) + ' '.join(self.attributes) + str(self.qty)
+        return retStr
     
     def find(self, query) -> bool:
         if isinstance(query, str):
             query = query.split()
         found = all(any(element in word for word in self.attributes) for element in query)
         return found
+    
+    def getContainerId(self) -> int:
+        return self._containerId
